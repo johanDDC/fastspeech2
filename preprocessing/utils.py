e428c30dcf3fa01a3e8b2fedb493f95ce909a7ef
johanDDC <ivan,peshekhonov@gmail.com>
2022-11-28 23:57:19 +0300

    Some fancy

---
+++ preprocessing/utils.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn.functional as F
+import os
+
+class Dict(dict):
+    def __init__(self, dct=None):
+        super().__init__()
+        dct = dict() if not dct else dct
+        for key, value in dct.items():
+            if hasattr(value, "keys"):
+                value = Dict(value)
+            self[key] = value
+
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
